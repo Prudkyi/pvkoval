fce10a3fe1d17f13adc1a0be5692270c6e110cf7
test_2022 <prudkyi87@gmail.com>
2022-10-02 22:21:29 +0200

    Готово відправлення email

--- src/files/mail/mail.php
+++ src/files/mail/mail.php
@@ -1,4 +1,5 @@
 <?php
+
   if (isset($_POST))
   {
     require_once('phpmailer/PHPMailerAutoload.php');
@@ -11,7 +12,8 @@
     $massageUser = htmlspecialchars($_POST['Massage']);
     $typeMess = $_POST['typeMess'];
 
-    if ($nameUser == '' || $tellUser == '' || $massegeUser == ''){
+
+    if ($nameUser == '' || $telUser == '' || $massageUser == ''){
         header("Location: http://pvkoval.com.ua/");
         exit( );
     }
@@ -31,26 +33,28 @@
     else {
         $themeMessage = "PVKoval Контактна Форма";
                 $message = "
-                <h1>PVKoval Заявка</h1>
+                <h1>PVKoval Контактна Форма</h1>
                 <ul>
                 <li><b>Ім'я: $nameUser</b></li>
                 <li><b>Телефон: $telUser</b></li>
-                <li><b>E-mail: $emailUser</b>/li>
+                <li><b>E-mail: $emailUser</b></li>
                 <li><b>Повідомлення: $massageUser</b></li>
                 </ul>
                 ";
     }
 
+
     $mail->isSMTP();
-        $mail->Host = 'linework.com.ua';
-        $mail->SMTPAuth = true;
-        $mail->Username = 'work@linework.com.ua';
-        $mail->Password = 'uj4DqX3g';
-        $mail->SMTPSecure = 'ssl';
-        $mail->Port = 465;
-        $mail->setFrom('linework.com.ua');
-        $mail->addAddress('prudkyi87@gmail.com');
-        $mail->isHTML(true);
+
+    $mail->Host = 'mwork.agency';
+    $mail->SMTPAuth = true;
+    $mail->Username = 'gojob@mwork.agency';
+    $mail->Password = 'q1234567890Serhii';
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port = 465;
+    $mail->setFrom('gojob@mwork.agency');
+    $mail->addAddress('prudkyi87@gmail.com');
+    $mail->isHTML(true);
 
     $mail->Subject = $themeMessage;
     $mail->Body    = $message;
@@ -59,9 +63,10 @@
     if(!$mail->send()) {
        echo 'Error';
     } else {
-        exit( );
+        echo 'Ok Email';
     }
 
 
   }
-?>
+
+
--- src/js/app.js
+++ src/js/app.js
@@ -249,7 +249,7 @@ $(document).ready(function() {
                 url: '../files/mail/mail.php',
                 type: 'post',
                 dataType: 'html',
-                data: { typeMess: typeThisMess, Name : arrDataForm[0], Phone : arrDataForm[1], Email : arrDataForm[2], Massage : arrDataForm[3]},
+                data: { typeMess : typeThisMess, Name : arrDataForm[0], Phone : arrDataForm[1], Email : arrDataForm[2], Massage : arrDataForm[3] },
                 success: function(data){
 
                 }
